f867bbc855a8e084b988c507664dce79c1cc5139
Jayant Malik <dev.jayantmalik@gmail.com>
2020-09-11 20:50:10 +0530

    added store persistance

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,24 @@ import thunk from "redux-thunk";
 import reducers from "./reducers";
 
 export default () => {
+  const initialState = JSON.parse(localStorage.getItem("store")) || {};
+
   const composeEnhancers =
     (typeof window !== "undefined" &&
       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
     compose;
 
   //   Compose is just for debugging.
-  return createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+  const store = createStore(
+    reducers,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk))
+  );
+
+  store.subscribe(() => {
+    const state = store.getState();
+    localStorage.setItem("store", JSON.stringify(state));
+  });
+
+  return store;
 };
